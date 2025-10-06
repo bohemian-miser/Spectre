2269b71c82798c14db2293e83c2a6e8362885463
bohemian-miser <158785140+bohemian-miser@users.noreply.github.com>
2025-10-06 06:24:34 +0000

    deleting stuff and fixing pages

--- web/src/sketch.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=sketch.d.ts.map
--- web/src/ui.d.ts
+++
@@ -1,4 +0,0 @@
-import * as p5 from 'p5';
-import { AppState } from './state';
-export declare function setupUI(p: p5, state: AppState, refreshCallback: () => void): void;
-//# sourceMappingURL=ui.d.ts.map
