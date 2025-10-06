0a9c8961c72b64d2d453cc1b15a58a7aec03ff26
bohemian-miser <158785140+bohemian-miser@users.noreply.github.com>
2025-10-06 06:42:16 +0000

    simplify workflow

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Spectre/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'p5': ['p5'],
+        },
+      },
+    },
+  },
 });
